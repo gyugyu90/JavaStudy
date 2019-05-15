6aca176f9d8fbcaedda4e9abcc21bce285430b42
gyugyu90 <kh2000park@gmail.com>
2019-05-15 16:26:35 +0900

    SmallestInteger(100%) => solution 추가

--- src/codility/tranining/MissingInteger.java
+++ src/codility/tranining/MissingInteger.java
@@ -1,8 +1,10 @@
 package codility.tranining;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-public class SmallestInteger {
+public class MissingInteger {
 
     private int solution(int[] A) {
         Arrays.sort(A); // O(n log n)
@@ -17,28 +19,43 @@ public class SmallestInteger {
         return ans;
     }
 
+    private int solution2(int[] A) {
+        Set<Integer> set = new HashSet<>();
+        for(int value: A) {
+            if(value > 0) set.add(value);
+        } // O(n)
+
+        for(int i=1;i<=100000;i++) { // O(n)
+            if(i == 100000 && set.contains(100000)) return 100001;
+            if(!set.contains(i)) return i;
+        }
+
+        return 1;
+    }
+
+
     public static void main(String[] args) {
 
-        SmallestInteger smallestInteger = new SmallestInteger();
+        MissingInteger missingInteger = new MissingInteger();
 
         int[] array1 = {1, 3, 6, 4, 1, 2};
-        int ans1 = smallestInteger.solution(array1);
+        int ans1 = missingInteger.solution2(array1);
         System.out.println(ans1); // expected: 5
 
         int[] array2 = {1,2,3};
-        int ans2 = smallestInteger.solution(array2);
+        int ans2 = missingInteger.solution2(array2);
         System.out.println(ans2); // expected: 4
 
         int[] array3 = {-1, -3};
-        int ans3 = smallestInteger.solution(array3);
+        int ans3 = missingInteger.solution2(array3);
         System.out.println(ans3); // expected: 1
 
         int[] array4 = {1, 1, 1};
-        int ans4 = smallestInteger.solution(array4);
+        int ans4 = missingInteger.solution2(array4);
         System.out.println(ans4); // expected: 2
 
         int[] array5 = {3, 2, 1};
-        int ans5 = smallestInteger.solution(array5);
+        int ans5 = missingInteger.solution2(array5);
         System.out.println(ans5); // expected: 4
     }
 }
