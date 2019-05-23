bed0f8bcef84dfbad69d0784118e763bd81daaa3
gyugyu90 <kh2000park@gmail.com>
2019-05-23 21:43:13 +0900

    버블 정렬

---
+++ src/datastructure/BubbleSort.java
@@ -0,0 +1,38 @@
+package datastructure;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BubbleSort {
+
+    // 1 + 2 + 3 + 4 + 5 + ... + (n - 1) => n * (n - 1) / 2 => O(n^2)
+    private void bubbleSort(int[] array) {
+        int temp;
+        for(int i = array.length - 1; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                if(array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testBubbleSort() {
+
+        int[] arr = new int[]{ 3, 1,4,5,2,6,7};
+        bubbleSort(arr);
+
+        for(int a : arr) {
+            System.out.print(a + "\t");
+        }
+        assertEquals(1, arr[0]);
+
+    }
+
+
+
+}
